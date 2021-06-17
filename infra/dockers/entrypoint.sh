73fc51b1d27edbc19c3ce4dae67a0ebc0382439e
Charles Marchand <charles-marchand@hotmail.fr>
2021-06-17 17:00:34 +0200

    feat(docker): docker now works

--- infra/dockers/entrypoint.sh
+++ infra/dockers/entrypoint.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
+# $1 for url
+# $2 for token
+
+export RUNNER_ALLOW_RUNASROOT="1"
 
 # Docker installation
-sudo sh -eux <<EOF
+sh -eux <<EOF
 # Install newuidmap & newgidmap binaries
 apt-get install -y uidmap
 EOF
 
 curl https://get.docker.com/ | bash
-dockerd-rootless-setuptool.sh install
 
 # Download github-runner
-mkdir actions-runner && cd actions-runner # Download the latest runner package
+mkdir /action-runner
+cd /action-runner
+
 curl -o actions-runner-linux-x64-2.278.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.278.0/actions-runner-linux-x64-2.278.0.tar.gz # Extract the installer
 tar xzf ./actions-runner-linux-x64-2.278.0.tar.gz
+ls
 
 # Configure
-cd actions-runner/
-./config.sh --url https://github.com/stractus/hello-github-actions --token ${token}
+./config.sh --url $1 --token $2
 
 # Run
 ./run.sh
